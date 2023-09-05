8f0cdc6132869191c981abd2cb8286394e2123bb
AlexisJavier1234 <143725720+AlexisJavier1234@users.noreply.github.com>
2023-09-05 09:39:49 -0500

    Add files via upload
    Ejercicios de la clase

---
+++ Clase.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package clase;
+
+/**
+ *
+ * @author labcca
+ */
+public class Clase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String Tarea1= " investiga main";
+        String Tarea2=", repaso de clase";
+        String Tarea3=", que es void";
+        String Tarea4=", pasos para algoritmo";
+        System.out.print("Lista de tareas:"+Tarea1);
+         System.out.print(Tarea2);
+          System.out.print(Tarea3);
+           System.out.print(Tarea4);
+    }
+    
+}
